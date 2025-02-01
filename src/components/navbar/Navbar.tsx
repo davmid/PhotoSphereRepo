cd0b331e44e38dace62acd6a0b908a7f9bccdb83
davmid <dawidm2614@gmail.com>
2025-02-01 19:37:51 +0100

    logo added

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,15 +3,15 @@ import './Navbar.css'
 import SearchIcon from '@mui/icons-material/Search';
 import PersonIcon from '@mui/icons-material/Person';
 import { exampleUsers } from '../../AssetsBase/Users';
+import logo from '../../photos/IGCLONE.png';
 
-const userId = 1;
+const userId = 2;
 const loggedUser = exampleUsers.find(user => user.id === userId);
 
 const Navbar: React.FC = () => {
   return (
     <div className='navbar'>
-      <img src='' alt='' className='logo' />
-      <h1>PhotoSphere</h1>
+      <img src={logo} alt="" className="logo" />
 
       <div className='search-box'>
         <input type='text' placeholder='Search'></input>
